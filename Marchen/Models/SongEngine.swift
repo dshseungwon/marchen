766f28d0d6e7ec24775c8077be65feef8d3a21e1
Seungwon Ju <dshseungwon@gmail.com>
2020-08-06 20:46:28 +0900

    Delete unnecessary comments.

--- Marchen/Models/SongEngine.swift
+++ Marchen/Models/SongEngine.swift
@@ -137,7 +137,6 @@ class SongEngine {
     }
     
     //MARK: - Private Methods
-    
     private func isAvailable() -> Bool {
         if key == nil || diatonicProgression == nil {
             return false
@@ -263,7 +262,6 @@ class SongEngine {
 
 extension SongEngine {
     // Methods for Observer pattern
-    
     func attachObserver(_ observer: Observer) {
         observers.append(observer)
     }
@@ -274,148 +272,3 @@ extension SongEngine {
         }
     }
 }
-
-//    private func playNoteSound(oscillator: AKOscillator, note: MIDINoteNumber) {
-//        // start from the correct note if amplitude is zero
-//        if oscillator.amplitude == 0 {
-//            oscillator.rampDuration = 0
-//        }
-//        oscillator.frequency = note.midiNoteToFrequency()
-//
-//        // Still use rampDuration for volume
-//        oscillator.rampDuration = currentRampDuration
-//        oscillator.amplitude = currentAmplitude
-//        oscillator.play()
-//    }
-
-//            let C3File = try AKAudioFile(readFileName: "C3.wav")
-//            let Db3File = try AKAudioFile(readFileName: "Db3.wav")
-//            let D3File = try AKAudioFile(readFileName: "D3.wav")
-//            let Eb3File = try AKAudioFile(readFileName: "Eb3.wav")
-//            let E3File = try AKAudioFile(readFileName: "E3.wav")
-//            let F3File = try AKAudioFile(readFileName: "F3.wav")
-//            let Gb3File = try AKAudioFile(readFileName: "Gb3.wav")
-//            let G3File = try AKAudioFile(readFileName: "G3.wav")
-//            let Ab3File = try AKAudioFile(readFileName: "Ab3.wav")
-//            let A3File = try AKAudioFile(readFileName: "A3.wav")
-//            let Bb3File = try AKAudioFile(readFileName: "Bb3.wav")
-//            let B3File = try AKAudioFile(readFileName: "B3.wav")
-//            let C4File = try AKAudioFile(readFileName: "C4.wav")
-//
-//            let Db4File = try AKAudioFile(readFileName: "Db4.wav")
-//            let D4File = try AKAudioFile(readFileName: "D4.wav")
-//            let Eb4File = try AKAudioFile(readFileName: "Eb4.wav")
-//            let E4File = try AKAudioFile(readFileName: "E4.wav")
-//            let F4File = try AKAudioFile(readFileName: "F4.wav")
-//            let Gb4File = try AKAudioFile(readFileName: "Gb4.wav")
-//            let G4File = try AKAudioFile(readFileName: "G4.wav")
-//            let Ab4File = try AKAudioFile(readFileName: "Ab4.wav")
-//            let A4File = try AKAudioFile(readFileName: "A4.wav")
-//            let Bb4File = try AKAudioFile(readFileName: "Bb4.wav")
-//            let B4File = try AKAudioFile(readFileName: "B4.wav")
-//            let C5File = try AKAudioFile(readFileName: "C5.wav")
-            
-//            let C3 = AKPlayer(audioFile: C3File)
-//            C3.isLooping = false
-//            C3.buffering = .always
-//            let Db3 = AKPlayer(audioFile: Db3File)
-//            Db3.isLooping = false
-//            Db3.buffering = .always
-//            let D3 = AKPlayer(audioFile: D3File)
-//            D3.isLooping = false
-//            D3.buffering = .always
-//            let Eb3 = AKPlayer(audioFile: Eb3File)
-//            Eb3.isLooping = false
-//            Eb3.buffering = .always
-//            let E3 = AKPlayer(audioFile: E3File)
-//            E3.isLooping = false
-//            E3.buffering = .always
-//            let F3 = AKPlayer(audioFile: F3File)
-//            F3.isLooping = false
-//            F3.buffering = .always
-//            let Gb3 = AKPlayer(audioFile: Gb3File)
-//            Gb3.isLooping = false
-//            Gb3.buffering = .always
-//            let G3 =  AKPlayer(audioFile: G3File)
-//            G3.isLooping = false
-//            G3.buffering = .always
-//            let Ab3 =  AKPlayer(audioFile: Ab3File)
-//            Ab3.isLooping = false
-//            Ab3.buffering = .always
-//            let A3 =  AKPlayer(audioFile: A3File)
-//            A3.isLooping = false
-//            A3.buffering = .always
-//            let Bb3 =  AKPlayer(audioFile: Bb3File)
-//            Bb3.isLooping = false
-//            Bb3.buffering = .always
-//            let B3 =  AKPlayer(audioFile: B3File)
-//            B3.isLooping = false
-//            B3.buffering = .always
-//            let C4 =  AKPlayer(audioFile: C4File)
-//            C4.isLooping = false
-//            C4.buffering = .always
-//
-//            let Db4 = AKPlayer(audioFile: Db4File)
-//            Db4.isLooping = false
-//            Db4.buffering = .always
-//            let D4 = AKPlayer(audioFile: D4File)
-//            D4.isLooping = false
-//            D4.buffering = .always
-//            let Eb4 = AKPlayer(audioFile: Eb4File)
-//            Eb4.isLooping = false
-//            Eb4.buffering = .always
-//            let E4 = AKPlayer(audioFile: E4File)
-//            E4.isLooping = false
-//            E4.buffering = .always
-//            let F4 = AKPlayer(audioFile: F4File)
-//            F4.isLooping = false
-//            F4.buffering = .always
-//            let Gb4 = AKPlayer(audioFile: Gb4File)
-//            Gb4.isLooping = false
-//            Gb4.buffering = .always
-//            let G4 =  AKPlayer(audioFile: G4File)
-//            G4.isLooping = false
-//            G4.buffering = .always
-//            let Ab4 =  AKPlayer(audioFile: Ab4File)
-//            Ab4.isLooping = false
-//            Ab4.buffering = .always
-//            let A4 =  AKPlayer(audioFile: A4File)
-//            A4.isLooping = false
-//            A4.buffering = .always
-//            let Bb4 =  AKPlayer(audioFile: Bb4File)
-//            Bb4.isLooping = false
-//            Bb4.buffering = .always
-//            let B4 =  AKPlayer(audioFile: B4File)
-//            B4.isLooping = false
-//            B4.buffering = .always
-//            let C5 =  AKPlayer(audioFile: C5File)
-//            C5.isLooping = false
-//            C5.buffering = .always
-            
-//            notePlayerDictionary.updateValue(C3, forKey: 48)
-//            notePlayerDictionary.updateValue(Db3, forKey: 49)
-//            notePlayerDictionary.updateValue(D3, forKey: 50)
-//            notePlayerDictionary.updateValue(Eb3, forKey: 51)
-//            notePlayerDictionary.updateValue(E3, forKey: 52)
-//            notePlayerDictionary.updateValue(F3, forKey: 53)
-//            notePlayerDictionary.updateValue(Gb3, forKey: 54)
-//            notePlayerDictionary.updateValue(G3, forKey: 55)
-//            notePlayerDictionary.updateValue(Ab3, forKey: 56)
-//            notePlayerDictionary.updateValue(A3, forKey: 57)
-//            notePlayerDictionary.updateValue(Bb3, forKey: 58)
-//            notePlayerDictionary.updateValue(B3, forKey: 59)
-//            notePlayerDictionary.updateValue(C4, forKey: 60)
-//
-//            notePlayerDictionary.updateValue(Db4, forKey: 61)
-//            notePlayerDictionary.updateValue(D4, forKey: 62)
-//            notePlayerDictionary.updateValue(Eb4, forKey: 63)
-//            notePlayerDictionary.updateValue(E4, forKey: 64)
-//            notePlayerDictionary.updateValue(F4, forKey: 65)
-//            notePlayerDictionary.updateValue(Gb4, forKey: 66)
-//            notePlayerDictionary.updateValue(G4, forKey: 67)
-//            notePlayerDictionary.updateValue(Ab4, forKey: 68)
-//            notePlayerDictionary.updateValue(A4, forKey: 69)
-//            notePlayerDictionary.updateValue(Bb4, forKey: 70)
-//            notePlayerDictionary.updateValue(B4, forKey: 71)
-//            notePlayerDictionary.updateValue(C5, forKey: 72)
-            
