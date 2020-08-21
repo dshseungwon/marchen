cd9bf4343aae753737d6d7949ddfaac81177f8b0
Seungwon Ju <dshseungwon@gmail.com>
2020-08-21 17:12:43 +0900

    Change storyboard & Add ChordEditView file

---
+++ Marchen/Views/ChordEditView.swift
@@ -0,0 +1,21 @@
+//
+//  ChordEditView.swift
+//  Marchen
+//
+//  Created by Seungwon Ju on 2020/08/21.
+//  Copyright © 2020 Seungwon Ju. All rights reserved.
+//
+
+import UIKit
+
+class ChordEditView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
