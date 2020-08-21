633f27a93419147b458036425ac0cfe2d72b3437
Seungwon Ju <dshseungwon@gmail.com>
2020-08-21 15:57:26 +0900

    Add new scenes

---
+++ Marchen/Controllers/SongEditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SongEditViewController.swift
+//  Marchen
+//
+//  Created by Seungwon Ju on 2020/08/21.
+//  Copyright © 2020 Seungwon Ju. All rights reserved.
+//
+
+import UIKit
+
+class SongEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
